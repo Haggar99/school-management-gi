1695730fe7c0f7175dce6c33d8e8a0afcbc0bfba
Haggar99 <amadydjongol@gmail.com>
2023-07-04 13:45:51 +0200

    feat:enseignant

--- src/controllers/enseignant.controller.ts
+++ src/controllers/enseignant.controller.ts
@@ -1,6 +1,7 @@
 import Enseignant, { EnseignantDoc } from "../models/enseignant.model";
 import { Request, Response } from "express";
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 
 
@@ -11,6 +12,8 @@ export const creeEnseignant = async (req: Request, res: Response ) => {
     console.log('enseignant: ',data);
 
     try {
+        // La fonction findOne() c'est une fonction qui nous
+        // permet de filtrer et de retourner un seul objet s'il existe
         const enseignant = await Enseignant.findOne({email});
 
         if (enseignant) {
@@ -18,10 +21,13 @@ export const creeEnseignant = async (req: Request, res: Response ) => {
                 message: 'Cet ensignant exist'
             })
         }
-        const newEnseignant = new Enseignant(data);
         const hash = await bcrypt.hash(data.password, 10);
         data.password = hash;
-        
+        const newEnseignant = new Enseignant(data);
+        console.log('newEnseignant: ',newEnseignant);
+
+        // la fonction save() nous permet d'enregistrer un
+        // ou plusieurs objet au niveau de la base de donnée.
         newEnseignant
         .save()
         .then(() => {
@@ -37,9 +43,60 @@ export const creeEnseignant = async (req: Request, res: Response ) => {
     }
 }
 
+export const loginEnseignant = async (req: Request, res: Response) => {
+    const userData = {
+        email: req.body.email,
+        password: req.body.password
+    }
+    const email: string = userData.email;  
+    try {
+        const enseignant: EnseignantDoc = await Enseignant.findOne({email});
 
-export const getEnseignant = async (req: Request, res: Response) => {
+        if (!enseignant) {
+            res.status(404).json({
+                message: "user not found!"
+            })
+        }
+        const isMatch = await bcrypt.compare(userData.password, enseignant.password);
 
+        if (!isMatch) {
+            res.status(400).json({
+                message: 'Mot de passe incorrect!'
+            })
+        }
+        const token = jwt.sign({
+            userId: enseignant._id,
+            email: enseignant.email
+        },
+        "AZERT1234",
+         {
+          expiresIn: "1h"
+        }
+        )
+        res.status(200).json({
+            message: 'Connecté',
+            token: token,
+            enseignant
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: 'Erreur: '+error
+        })
+    }
+}
 
-    console.log("get Enseignant");
+export const getEnseignants = async (req: Request, res: Response) => {
+
+    try {
+        const enseignants: EnseignantDoc[] = await Enseignant.find()
+
+        res.status(201).json({
+            enseignants
+        });
+        
+    } catch (error) {
+        res.status(500).json({
+            message: 'erreur: ' + error
+        })
+    }
 }
--- src/routes/enseignant.routes.ts
+++ src/routes/enseignant.routes.ts
@@ -5,6 +5,7 @@ import *as enseignantCtrl from '../controllers/enseignant.controller';
 const enseignantRouter = express.Router();
 
 enseignantRouter.post('/new-enseignant', enseignantCtrl.creeEnseignant);
-enseignantRouter.get('/',enseignantCtrl.getEnseignant);
+enseignantRouter.get('/',enseignantCtrl.getEnseignants);
+enseignantRouter.post('/login-enseignant', enseignantCtrl.loginEnseignant);
 
 module.exports = enseignantRouter;
